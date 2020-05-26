39ecec6860e5d84f8d02a866df2cf260ac97f0b3
Jess <jesszeroual@gmail.com>
2020-05-26 12:20:50 +0200

    compress response query

---
+++ web-app/build/precache-manifest.3d1e6e4803d9da7a48b87e2f3019f978.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f59374a71a46482f49668d8d51de039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39294fa9cd6ef947e5ad",
+    "url": "/static/js/2.f0b0e0d5.chunk.js"
+  },
+  {
+    "revision": "7ba484bf652c7747bc62",
+    "url": "/static/js/main.821c14e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
